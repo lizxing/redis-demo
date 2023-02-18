4ed14b2312f3d82d6cc1650635d2b472b10e1033
li.zhenxin <1483588183@qq.com>
2023-02-18 10:09:08 +0800

    1

---
+++ src/main/java/com/lizxing/redis/demo/service/impl/Test.java
@@ -0,0 +1,4 @@
+package com.lizxing.redis.demo.service.impl;
+
+public class Test {
+}
---
+++ src/main/java/com/lizxing/redis/demo/service/impl/Test1.java
@@ -0,0 +1,12 @@
+package com.lizxing.redis.demo.service.impl;
+
+/**
+ * @description:
+ * @author: lizxing
+ * @create: 2023/2/17 9:56
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        int a = 1;
+    }
+}
